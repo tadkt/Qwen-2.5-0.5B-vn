f54cbd4da888d9d9f566e8d25c89fac95db7c089
Tadkt <thaidoankien@gmail.com>
2025-03-21 11:03:01 +0700

    Train works, but extra vocab not detected

--- train/train.py
+++ train/train.py
@@ -8,90 +8,94 @@ from swift.trainers import Seq2SeqTrainer, Seq2SeqTrainingArguments
 from functools import partial
 
 logger = get_logger()
-seed_everything(42)
+def train():
+    seed_everything(42)
 
 
-# Hyperparameters for training
-# model
-model_id_or_path = 'Qwen/Qwen2.5-0.5B-Instruct'  # model_id or model_path
-system = 'You are a helpful assistant.'
-output_dir = 'output'
+    # Hyperparameters for training
+    # model
+    model_id_or_path = 'Qwen/Qwen2.5-0.5B-Instruct'  # model_id or model_path
+    system = 'You are a helpful assistant.'
+    output_dir = 'output'
 
-# dataset
-dataset = "./data/proccessed_data.jsonl"  # dataset_id or dataset_path
-data_seed = 42
-max_length = 2048
-split_dataset_ratio = 0.01  # Split validation set
-num_proc = 4  # The number of processes for data loading.
+    # dataset
+    dataset = "./data/proccessed_data.jsonl"  # dataset_id or dataset_path
+    data_seed = 42
+    max_length = 2048
+    split_dataset_ratio = 0.01  # Split validation set
+    num_proc = 4  # The number of processes for data loading.
 
-# lora
-lora_rank = 8
-lora_alpha = 32
+    # lora
+    lora_rank = 8
+    lora_alpha = 32
 
-# training_args
-training_args = Seq2SeqTrainingArguments(
-    output_dir=output_dir,
-    learning_rate=1e-4,
-    per_device_train_batch_size=1,
-    per_device_eval_batch_size=1,
-    gradient_checkpointing=True,
-    weight_decay=0.1,
-    lr_scheduler_type='cosine',
-    warmup_ratio=0.05,
-    report_to=['wandb'],
-    logging_first_step=True,
-    save_strategy='steps',
-    save_steps=50,
-    eval_strategy='steps',
-    eval_steps=50,
-    gradient_accumulation_steps=16,
-    num_train_epochs=1,
-    metric_for_best_model='loss',
-    save_total_limit=5,
-    logging_steps=5,
-    dataloader_num_workers=1,
-    data_seed=data_seed,
-)
+    # training_args
+    training_args = Seq2SeqTrainingArguments(
+        output_dir=output_dir,
+        learning_rate=1e-4,
+        per_device_train_batch_size=1,
+        per_device_eval_batch_size=1,
+        gradient_checkpointing=True,
+        weight_decay=0.1,
+        lr_scheduler_type='cosine',
+        warmup_ratio=0.05,
+        report_to=['wandb'],
+        logging_first_step=True,
+        save_strategy='steps',
+        save_steps=50,
+        eval_strategy='steps',
+        eval_steps=50,
+        gradient_accumulation_steps=16,
+        num_train_epochs=1,
+        metric_for_best_model='loss',
+        save_total_limit=5,
+        logging_steps=5,
+        dataloader_num_workers=1,
+        data_seed=data_seed,
+    )
 
-output_dir = os.path.abspath(os.path.expanduser(output_dir))
-logger.info(f'output_dir: {output_dir}')
+    output_dir = os.path.abspath(os.path.expanduser(output_dir))
+    logger.info(f'output_dir: {output_dir}')
 
-# Obtain the model and template, and add a trainable Lora layer on the model.
-model, tokenizer = get_model_tokenizer(model_id_or_path)
-logger.info(f'model_info: {model.model_info}')
-template = get_template(model.model_meta.template, tokenizer, default_system=system, max_length=max_length)
-template.set_mode('train')
+    # Obtain the model and template, and add a trainable Lora layer on the model.
+    model, tokenizer = get_model_tokenizer(model_id_or_path)
+    logger.info(f'model_info: {model.model_info}')
+    template = get_template(model.model_meta.template, tokenizer, default_system=system, max_length=max_length)
+    template.set_mode('train')
 
-target_modules = find_all_linears(model)
-lora_config = LoraConfig(task_type='CAUSAL_LM', r=lora_rank, lora_alpha=lora_alpha,
-                         target_modules=target_modules)
-model = Swift.prepare_model(model, lora_config)
-logger.info(f'lora_config: {lora_config}')
+    target_modules = find_all_linears(model)
+    lora_config = LoraConfig(task_type='CAUSAL_LM', r=lora_rank, lora_alpha=lora_alpha,
+                            target_modules=target_modules)
+    model = Swift.prepare_model(model, lora_config)
+    logger.info(f'lora_config: {lora_config}')
 
-# Print model structure and trainable parameters.
-logger.info(f'model: {model}')
-model_parameter_info = get_model_parameter_info(model)
-logger.info(f'model_parameter_info: {model_parameter_info}')
+    # Print model structure and trainable parameters.
+    logger.info(f'model: {model}')
+    model_parameter_info = get_model_parameter_info(model)
+    logger.info(f'model_parameter_info: {model_parameter_info}')
 
-train_dataset, val_dataset = load_dataset(dataset, split_dataset_ratio=split_dataset_ratio, num_proc=num_proc, seed=data_seed)
-logger.info(f'train_dataset: {train_dataset}')
-logger.info(f'val_dataset: {val_dataset}')
-logger.info(f'train_dataset[0]: {train_dataset[0]}')
+    train_dataset, val_dataset = load_dataset(dataset, split_dataset_ratio=split_dataset_ratio, num_proc=num_proc, seed=data_seed)
+    logger.info(f'train_dataset: {train_dataset}')
+    logger.info(f'val_dataset: {val_dataset}')
+    logger.info(f'train_dataset[0]: {train_dataset[0]}')
 
-train_dataset = EncodePreprocessor(template=template)(train_dataset, num_proc=num_proc)
-val_dataset = EncodePreprocessor(template=template)(val_dataset, num_proc=num_proc)
-logger.info(f'encoded_train_dataset[0]: {train_dataset[0]}')
+    train_dataset = EncodePreprocessor(template=template)(train_dataset, num_proc=num_proc)
+    val_dataset = EncodePreprocessor(template=template)(val_dataset, num_proc=num_proc)
+    logger.info(f'encoded_train_dataset[0]: {train_dataset[0]}')
 
-model.enable_input_require_grads()  # Compatible with gradient checkpointing
-trainer = Seq2SeqTrainer(
-    model=model,
-    args=training_args,
-    data_collator=template.data_collator,
-    train_dataset=train_dataset,
-    eval_dataset=val_dataset,
-    template=template,
-)
-trainer.train()
+    model.enable_input_require_grads()  # Compatible with gradient checkpointing
+    trainer = Seq2SeqTrainer(
+        model=model,
+        args=training_args,
+        data_collator=template.data_collator,
+        train_dataset=train_dataset,
+        eval_dataset=val_dataset,
+        template=template,
+    )
+    trainer.train()
 
-last_model_checkpoint = trainer.state.last_model_checkpoint
-logger.info(f'last_model_checkpoint: {last_model_checkpoint}')
+    last_model_checkpoint = trainer.state.last_model_checkpoint
+    logger.info(f'last_model_checkpoint: {last_model_checkpoint}')
+
+if __name__ == "__main__":
+    train()
